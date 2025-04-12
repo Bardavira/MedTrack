2806e30884309e95c378500fbbed42bb5bed0ba4
random_naomi <naomicaccialucca@gmail.com>
2025-04-12 01:58:59 -0300

    teste com nesting do arquivo do menu

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/partials/menu.blade.php -->
+<nav class="flex items-center justify-start gap-6">
+    <a href="/" class="text-sm text-[#1b1b18] dark:text-[#EDEDEC] hover:underline">Home</a>
+    <a href="/about" class="text-sm text-[#1b1b18] dark:text-[#EDEDEC] hover:underline">Prontuarios</a>
+    <a href="/services" class="text-sm text-[#1b1b18] dark:text-[#EDEDEC] hover:underline">Salas</a>
+    <a href="/contact" class="text-sm text-[#1b1b18] dark:text-[#EDEDEC] hover:underline">Medicos</a>
+</nav>
