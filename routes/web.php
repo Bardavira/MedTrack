2a4b3ed6e9e58d42d0129742840d62b0887ccaee
Guilherme Zamorreño <guilhermezamorreno@gmail.com>
2025-04-28 19:20:43 -0300

    Adicionado o controller, form requests e rotas envolvendo as alas

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\GoogleLoginController;
 use App\Http\Controllers\UbsUnitController;
+use App\Http\Controllers\UbsWingController;
 use App\Http\Middleware\RedirectUnauthenticatedUser;
 
 Route::get('/', function () {
@@ -15,7 +16,7 @@ Route::get('/login', function () {
 Route::get('/google/redirect', [GoogleLoginController::class, 'redirectToGoogle'])->name('google.redirect');
 Route::get('/google/callback', [GoogleLoginController::class, 'handleGoogleCallback'])->name('google.callback');
 Route::middleware(RedirectUnauthenticatedUser::class)->group(function () {
-    
+    Route::get('/user', [UserController::class, 'index'])->name('index');
 
     Route::delete('/user/{id}', [UserController::class, 'delete'])->name('delete');
 
@@ -29,6 +30,12 @@ Route::get('/unitsdata', [UbsUnitController::class, 'store'])->name('unitsstore'
 
 Route::get('/units/{id}/put', [UbsUnitController::class, 'update'])->name('unitsupdate');
 
-Route::get('units/{id}', [UbsUnitController::class, 'delete'])->name('unitsdelete');
+Route::get('/units/{id}', [UbsUnitController::class, 'delete'])->name('unitsdelete');
 
-Route::get('/user', [UserController::class, 'index'])->name('index');
+Route::get('/wings', [UbsWingController::class, 'index'])->name('wingsindex');
+
+Route::get('/wingsdata', [UbsWingController::class, 'store'])->name('wingsstore');
+
+Route::get('/wings/{id}/put', [UbsWingController::class, 'update'])->name('wingsupdate');
+
+Route::get('/wings/{id}', [UbsWingController::class, 'delete'])->name('wingsdelete');
